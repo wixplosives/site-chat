4e7a947a83e181263b31b69a13691fa0c111d10f
alisaLachman <alisal@wix.com>
2023-06-01 15:15:44 +0300

    input

---
+++ src/_codux/boards/chat-input/chat-input.board.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createBoard } from '@wixc3/react-board';
+import { ChatInput } from '../../../components/chat-input/chat-input';
+
+export default createBoard({
+    name: 'ChatInput',
+    Board: () => <ChatInput />
+});
---
+++ src/components/chat-input/chat-input.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import styles from './chat-input.module.scss';
+
+export interface ChatInputProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const ChatInput = ({ className }: ChatInputProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <input className={styles.input} />
+        </div>
+    );
+};
