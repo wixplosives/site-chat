b6dd87ffe64d32a230129393851b0b67a0d1bce5
Nadav Abrahami <nadav_ab@wix.com>
2023-06-01 18:35:16 +0300

    merged master

--- src/components/chat-icons/chat-icons.tsx
+++ src/components/chat-icons/chat-icons.tsx
@@ -4,26 +4,19 @@ import ChatInput_module from '../chat-input/chat-input.module.scss';
 
 export interface ChatIconsProps {
     className?: string;
+    onClick?: () => void;
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const ChatIcons = ({ className }: ChatIconsProps) => {
+export const ChatIcons = ({ className, onClick }: ChatIconsProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <div>
-                <img
-                    src="   https://cdn-icons-png.flaticon.com/512/739/739249.png "
-                    className={classNames(styles.AddImage, styles['margin-4'])}
-                />
-                <img
-                    src="   https://cdn-icons-png.flaticon.com/512/659/659998.png "
-                    className={styles.AddImage}
-                />
-            </div>
+            <div></div>
             <img
+                onClick={onClick}
                 src="   https://cdn-icons-png.flaticon.com/512/786/786205.png "
                 className={styles.AddImage}
             />
--- src/components/chat-input/chat-input.tsx
+++ src/components/chat-input/chat-input.tsx
@@ -4,20 +4,28 @@ import { ChatIcons } from '../chat-icons/chat-icons';
 
 export interface ChatInputProps {
     className?: string;
+    sendText: () => void;
+    text: string;
+    onTextChange: (text: string) => void;
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const ChatInput = ({ className }: ChatInputProps) => {
+export const ChatInput = ({ className, sendText, onTextChange, text }: ChatInputProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles.DivInput}>
-                <input placeholder="Send me a massage" className={styles.input} />
-                
+                <input
+                    placeholder="Send me a massage"
+                    className={styles.input}
+                    value={text}
+                    onChange={(ev) => onTextChange(ev.target.value)}
+                    onSubmit={sendText}
+                />
             </div>
-            <ChatIcons />
+            <ChatIcons onClick={sendText} />
         </div>
     );
 };
