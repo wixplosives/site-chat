80d47b4b90365c7b3b3e27b3b8a23110a15661e8
Nadav Abrahami <nadav_ab@wix.com>
2023-06-01 17:25:51 +0300

    work on custom element

--- src/components/chat/chat-wrapper.tsx
+++ src/components/chat/chat-wrapper.tsx
@@ -1,17 +1,27 @@
-import {Chat} from './chat'
-import {ChatMessage, AvatarMessage} from '../types'
-import {AvatarMessageView} from '../avatar-message/avatar-message'
-import {UserMessageView} from '../user-message/user-message'
-import {useState} from 'react'
+import { Chat } from './chat';
+import { ChatMessage, AvatarMessage } from '../types';
+import { AvatarMessageView } from '../avatar-message/avatar-message';
+import { UserMessageView } from '../user-message/user-message';
+import { useState } from 'react';
 
-
-export const WrappedChat = ({messages}: {messages:Array<ChatMessage>})=>{
-    const [currentMessages, setMessages] = useState(messages)
-    return <Chat messages={currentMessages} views={{
-        [AvatarMessageView.kind]: AvatarMessageView,
-        [UserMessageView.kind]: UserMessageView
-    }} onUserMessage={(text)=>setMessages([...currentMessages, {
-        kind:'user-message',
-        text
-    }])}/>
-}
+export const WrappedChat = <T extends ChatMessage>({ messages }: { messages: Array<T> }) => {
+    const [currentMessages, setMessages] = useState(messages);
+    return (
+        <Chat
+            messages={currentMessages}
+            views={{
+                [AvatarMessageView.kind]: AvatarMessageView,
+                [UserMessageView.kind]: UserMessageView,
+            }}
+            onUserMessage={(text) =>
+                setMessages([
+                    ...currentMessages,
+                    {
+                        kind: 'user-message',
+                        text,
+                    } as any,
+                ])
+            }
+        />
+    );
+};
